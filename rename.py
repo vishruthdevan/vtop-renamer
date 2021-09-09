e011e6392f16e83797e4a848b8b101a61096aeaa
Vishruth Devan <vishruthdevan@gmail.com>
2021-09-09 15:27:06 +0530

    Modify script for new naming convention

--- rename.py
+++ rename.py
@@ -27,18 +27,31 @@ def setup(path):
 def rename(path):
     namelist = list()
     datelist = list()
-    pattern = r"(\d\d)-(\w\w\w)-(\d\d\d\d)(.*)"
+    pattern1 = r"(\d\d)-(\w\w\w)-(\d\d\d\d)(.*)"
+    pattern2 = r"(\d\d)-(\d\d)-(\d\d\d\d)(.*)"
     for i in os.listdir(path):
-        if(m := re.search(pattern, i)):
+        if(m := re.search(pattern1, i)):
             d = date(int(m.group(3)), int(months.index(m.group(2)))+1, int(m.group(1)))
             oldName = str(path) + "\\" + i
             namelist.append(oldName)
             datelist.append(d)
+            pattern = pattern1
+        
+        if(m := re.search(pattern2, i)):
+            d = date(int(m.group(3)), int(m.group(2)), int(m.group(1)))
+            oldName = str(path) + "\\" + i
+            namelist.append(oldName)
+            datelist.append(d)
+            pattern = pattern2
+    
     sorted_names = sorted(list(zip(namelist, datelist)), key = lambda x: x[1])
     count = 1
     for i, j in sorted_names:
         m = re.search(pattern, i)
-        newName = str(path) + '\\' + str(count) + '. ' + m.group(2) + '-'+ m.group(1) + '-' + m.group(3) + m.group(4)
+        if pattern == pattern1:
+            newName = str(path) + '\\' + str(count) + '. ' + m.group(2) + '-'+ m.group(1) + '-' + m.group(3) + m.group(4)
+        else:
+            newName = str(path) + '\\' + str(count) + '. ' + months[int(m.group(2))-1] + '-'+ m.group(1) + '-' + m.group(3) + m.group(4)
         print(newName)
         shutil.move(i, newName)
         count+=1
