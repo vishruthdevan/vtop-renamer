da1abb3ba403f7d606ee2962a6a2821912e732df
Vishruth Devan <vishruthdevan@gmail.com>
2025-06-28 11:43:03 -0400

    Use date module to sort files by date

--- rename.py
+++ rename.py
@@ -4,7 +4,7 @@ from datetime import date
 from pprint import pprint
 import zipfile
 
-
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 def setup():
     notes = Path(' '.join(sys.argv[1:])).absolute()
     notes_zip = zipfile.ZipFile(notes)
@@ -14,17 +14,28 @@ def setup():
     # if not os.path.exists(str(path)+ '-pretty'):
     #     os.mkdir(str(path)+'-pretty')
     notes_zip.extractall(path)
-    shutil.copytree(path, str(path)+'_pretty')
+    shutil.copytree(path, str(path)+'_pretty', dirs_exist_ok=True)
+
     return str(path)+'_pretty'
 
 def rename(path):
+    namelist = list()
+    datelist = list()
+    pattern = r"(\d\d)-(\w\w\w)-(\d\d\d\d)(.*)"
     for i in os.listdir(path):
-        pattern = r"(\d\d)-(\w\w\w)-(\d\d\d\d)(.*)"
-        if(re.search(pattern, i)):
-            m = re.search(pattern, i)
-            newName = str(path) + '\\' + m.group(2) + '-'+ m.group(1) + '-' + m.group(3) + m.group(4)
-            oldName = str(path) + '\\' + i
-            shutil.move(oldName, newName) #will be uncommented after debug
+        if(m := re.search(pattern, i)):
+            d = date(int(m.group(3)), int(months.index(m.group(2)))+1, int(m.group(1)))
+            oldName = str(path) + "\\" + i
+            namelist.append(oldName)
+            datelist.append(d)
+    sorted_names = sorted(list(zip(namelist, datelist)), key = lambda x: x[1])
+    count = 1
+    for i, j in sorted_names:
+        m = re.search(pattern, i)
+        newName = str(path) + '\\' + str(count) + '. ' + m.group(2) + '-'+ m.group(1) + '-' + m.group(3) + m.group(4)
+        shutil.move(i, newName)
+        count+=1
+    
 
 if __name__=="__main__":
     rename(setup())
