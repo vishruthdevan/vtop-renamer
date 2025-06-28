e2498abb9d0fed651265bc8ba140e3b360307cb3
Vishruth Devan <vishruthdevan@gmail.com>
2025-06-28 11:43:04 -0400

    Fixed errors renaming files with the new naming format

--- rename.py
+++ rename.py
@@ -35,22 +35,19 @@ def rename(path):
             oldName = str(path) + "\\" + i
             namelist.append(oldName)
             datelist.append(d)
-            pattern = pattern1
         
         if(m := re.search(pattern2, i)):
             d = date(int(m.group(3)), int(m.group(2)), int(m.group(1)))
             oldName = str(path) + "\\" + i
             namelist.append(oldName)
             datelist.append(d)
-            pattern = pattern2
     
     sorted_names = sorted(list(zip(namelist, datelist)), key = lambda x: x[1])
     count = 1
     for i, j in sorted_names:
-        m = re.search(pattern, i)
-        if pattern == pattern1:
+        if(m := re.search(pattern1, i)):
             newName = str(path) + '\\' + str(count) + '. ' + m.group(2) + '-'+ m.group(1) + '-' + m.group(3) + m.group(4)
-        else:
+        elif (m := re.search(pattern2, i)):
             newName = str(path) + '\\' + str(count) + '. ' + months[int(m.group(2))-1] + '-'+ m.group(1) + '-' + m.group(3) + m.group(4)
         print(newName)
         shutil.move(i, newName)
