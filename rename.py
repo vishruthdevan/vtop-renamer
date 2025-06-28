1e68424f4dc8ab052c74fa533b154921fd30b518
Vishruth Devan <vishruthdevan@gmail.com>
2025-06-28 11:43:03 -0400

    Files will be added to new dir

--- rename.py
+++ rename.py
@@ -5,11 +5,14 @@ from pprint import pprint
 
 
 pattern = r"(\d\d)-(\w\w\w)-(\d\d\d\d)(.*)"
-path = Path(' '.join(sys.argv[1:]))
-for i in os.listdir(path):
+path = Path(' '.join(sys.argv[1:])).absolute()
+if not os.path.exists(str(path)+'-pretty'):
+    os.mkdir(str(path)+'-pretty')
 
+for i in os.listdir(path):
     if(re.search(pattern, i)):
         m = re.search(pattern, i)
-        newName = str(path) + '\\' + m.group(2) + '-'+ m.group(1) + '-' + m.group(3) + m.group(4)
+        newName = str(path) + '-pretty\\' + m.group(2) + '-'+ m.group(1) + '-' + m.group(3) + m.group(4)
         oldName = str(path) + '\\' + i
-        shutil.move(oldName, newName)
+        print(newName)
+        #shutil.move(oldName, newName) will be uncommented after debug
